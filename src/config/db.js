f3db6ab9bdef62e3366913c8fc2cf1f6f3363569
JavierShepard <comander.javier@gmail.com>
2024-07-12 21:30:56 -0300

    para render

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2');
 
-
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'Javi',
-  password: 'J4vier2022',
-  database: 'comision24135'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 connection.connect((err) => {
